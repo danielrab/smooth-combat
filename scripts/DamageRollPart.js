fd2b599ae7be3ce11f8694fc987944cb14cec6c6
danielrab <s17b1_rabinovich@179.ru>
2020-10-28 13:07:41 +0200

    finishing transition to templates

--- scripts/DamageRollPart.js
+++ scripts/DamageRollPart.js
@@ -4,16 +4,12 @@ class DamageRollPartHandler {
     this.type = type;
   }
 
-  async render() {
-    const rollElement = $(await this.roll.render());
-    const diceTotal = rollElement.find('.dice-total');
-    window.test = diceTotal;
-    diceTotal[0].innerHTML += ` ${this.type} <button class="damage-button"><i class="fas fa-user-minus" title="Click to apply full damage to selected token(s)."></i></button>`;
-    return rollElement[0].outerHTML;
-  }
-
   get total() {
     return this.roll.total;
+  }
+
+  get formula() {
+    return this.roll._formula;
   }
 
   getDamage(target) {
--- scripts/html-generation.js
+++ scripts/html-generation.js
@@ -1,23 +1,3 @@
 export default async function attackResultHTML(result) {
-  try {
-    const attackRollHTML = await result.attackRoll.render();
-    const damageRollHTML = await result.damageRoll.render();
-    const hit = result.attackRoll.hits(result.target);
-    if (hit) {
-      return `
-${attackRollHTML}
-${result.actor.name} hits ${result.target.actor.name} with ${result.item.name} </br>
-dealing
-${damageRollHTML}
-`;
-    }
-    return `
-${await result.attackRoll.render()}
-${result.actor.name} misses ${result.target.actor.name}
-  `;
-  }
-  catch (e) {
-    ui.notifications.error(`an error occured in html-generation.attackResultHTML: ${e}`);
-    return '';
-  }
+  return renderTemplate('./modules/smooth-combat/templates/attackResult.hbs', result);
 }
